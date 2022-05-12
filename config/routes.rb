0c339ccad7c38a11fd36f539d8636cce7d5c80ea
mconiaris <mconiaris@gmail.com>
2022-05-12 08:58:03 -0400

    Defined root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root "welcome#home"
   get 'welcome/home'
   get 'welcome/about'
   get 'welcome/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
