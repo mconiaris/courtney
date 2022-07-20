9c008f83d601a0e902f8b1656bdccd4c244d4d32
mconiaris <mconiaris@gmail.com>
2022-07-20 12:53:56 -0400

    Modified routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#home"
-  get 'welcome/home'
-  get 'welcome/about'
-  get 'welcome/contact'
+  get 'home', to: 'welcome#home'
+  get 'about', to: 'welcome#about'
+  get 'contact', to: 'welcome#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
